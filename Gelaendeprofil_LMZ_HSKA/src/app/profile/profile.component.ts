b5e78cf90f019e5c1155cc6f1ceb3140125ba8cb
Fuad Amin <fube1015@hs-karlsruhe.de>
2017-05-04 14:42:25 +0200

    fuad

--- Gelaendeprofil_LMZ_HSKA/src/app/profile/profile.component.ts
+++ Gelaendeprofil_LMZ_HSKA/src/app/profile/profile.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+
+
+import * as D3 from 'd3/index';
+import * as Moment from 'moment';
+import { LeafletMap } from '../LeafletMap.component';
+
 @Component({
   selector: 'profile',
   templateUrl: 'profile.component.html',
   styleUrls: ['profile.component.css']
 })
 export class ProfileComponent { name = 'Angular'; }
+
+
