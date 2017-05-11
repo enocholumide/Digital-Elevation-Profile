f8724d39362236bfe1be506a245c964f60234e4e
Fuad Amin <fube1015@hs-karlsruhe.de>
2017-05-11 16:00:00 +0200

    fuad

--- Gelaendeprofil_LMZ_HSKA/src/app/profile/profile.component.ts
+++ Gelaendeprofil_LMZ_HSKA/src/app/profile/profile.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnChanges, AfterViewInit, NgZone, ViewChild, ElementRef, Input, ViewEncapsulation, AfterViewChild } from '@angular/core';
 import * as d3 from 'd3';
-
+//const d3=require('d3');
+import * as moment from 'moment';
 import '../shared/d3/build/d3.js';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit, OnChanges {
-  @ViewChild('chart') private chartContainer: ElementRef;
+export class ProfileComponent implements OnInit, OnChanges, AfterViewChild {
+  @ViewChild('chart') private element: ElementRef;
   @Input() private data = [];
-  private margin: any = { top: 0, bottom: 0, left: 0, right: 0};
+  
+  private host;
+  private svg;
+  private margin: any = { top: 15, bottom: 15, left: 15, right: 15};
   private chart: any;
   private width: number;
   private height: number;
@@ -21,7 +26,11 @@ export class ProfileComponent implements OnInit, OnChanges {
   private xAxis: any;
   private yAxis: any;
 
-  constructor() { }
+  private HTMLElement: HTMLElement;
+  t:any;
+  update: any;
+
+  constructor(private zone: NgZone) { }
 
   ngOnInit() {
     //console.log(this.chartContainer.nativeElement);
@@ -43,21 +52,31 @@ export class ProfileComponent implements OnInit, OnChanges {
     //console.log(element)
     this.width = 500;
     this.height = 300;
+   // this.t = d3.transition().duration(10);
     let svg = d3.select(element).append('svg')
-      .attr('width', "100%")
-      .attr('height', "100%");
+      .attr('width', "90%")
+      .attr('height', "90%");
 
     // chart plot area
     this.chart = svg.append('g')
       .attr('class', 'bars')
+      .attr('class', 'line')
       .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
+
+    let line = d3.line()
+    .x(d => this.xScale(d[0]))
+    .y(d => this.yScale(d[1]))
+    .curve(d3.curveCatmullRom.alpha(0.5));
+
+
 
     // define X & Y domains
     let xDomain = this.data.map(d => d[0]);
     let yDomain = [0, d3.max(this.data, d => d[1])];
 
     // create scales
-    this.xScale = d3.scaleLinear().domain(xDomain).range([0, this.width]);
+    this.xScale = d3.scaleBand().padding(0.1).domain(xDomain).rangeRound([0, this.width]);
+    //this.xScale = d3.line[this.chart.d[0], this.chart.d[1]];
     this.yScale = d3.scaleLinear().domain(yDomain).range([this.height, 0]);
 
     // bar colors
@@ -84,12 +103,13 @@ export class ProfileComponent implements OnInit, OnChanges {
 
     let update = this.chart.selectAll('.bar')
       .data(this.data);
-
+      
     // remove exiting bars
     update.exit().remove();
 
     // update existing bars
     this.chart.selectAll('.bar').transition()
+    this.chart.selectAll('.line').transition()
       .attr('x', d => this.xScale(d[0]))
       .attr('y', d => this.yScale(d[1]))
       .attr('width', d => this.xScale.bandwidth())
@@ -100,7 +120,11 @@ export class ProfileComponent implements OnInit, OnChanges {
     update
       .enter()
       .append('rect')
+    //  .append('path')
       .attr('class', 'bar')
+    //  .attr('class', 'line')
+      //.transition(this.t)
+      //.attr('d', d => line(this.data))
       .attr('x', d => this.xScale(d[0]))
       .attr('y', d => this.yScale(0))
       .attr('width', this.xScale.bandwidth())
