4c0c9d204ff701aed01ecb4515fb909132e70f5c
SunStormRain <fuerstanja@gmx.de>
2017-06-12 18:14:27 +0200

    Translate Marker, Label edit

--- src/app/leafletmap/leafletmap.component.ts
+++ src/app/leafletmap/leafletmap.component.ts
@@ -294,8 +294,8 @@ export class LeafletmapComponent implements OnInit {
       let infoBox = document.createElement('a');
       infoBox.innerHTML = 
       `   <form id="edit_labels" role="form">
-                  <span type="button" class="input-group-addon btn btn-primary active" id="labelEdit">Edit label</span>
-                  <input id="edit_labels" type="text" class="form-control" placeholder="e.g Point A">     
+                  <span type="button" class="input-group-addon btn btn-primary active" id="labelEdit">Label bearbeiten</span>
+                  <input id="edit_labels" type="text" class="form-control" placeholder="z.B. Punkt A">     
           </form>  
       `
       for (let i = 0; i < layer._latlngs.length; i++) {
